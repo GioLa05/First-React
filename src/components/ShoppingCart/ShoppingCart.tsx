82c44f38ece92e7e31e0cf1ab986c14ebed1698f
GioLa05 <giorgich535@gmail.com>
2025-03-18 23:26:02 +0400

    18 March

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -30,25 +30,27 @@ const ShoppingCart = (props: Props) => {
             <div className={styles.info}>
               <div className={styles.infoLeft}>
                 <div className={styles.text}>
-                  <p className={styles.h1}>
-                  {product.name}
-                  </p>
+                  <p className={styles.h1}>{product.name}</p>
                   <p>{product.id}</p>
                 </div>
               </div>
               <div className={styles.infoRight}>
                 <div className={styles.counter}>
-                  <img
-                    className={styles.minus}
-                    src="/assets/icons/No Edit.svg"
-                    alt="no-edit"
-                  />
-                  <button>1</button>
-                  <img
-                    className={styles.plus}
-                    src="/assets/icons/Edit.svg"
-                    alt="edit"
-                  />
+                  <button className={styles.counterButton}>
+                    <img
+                      className={styles.minus}
+                      src="/assets/icons/No Edit.svg"
+                      alt="no-edit"
+                    />
+                  </button>
+                  <button className={styles.counterNumber}>1</button>
+                  <button className={styles.counterButton}>
+                    <img
+                      className={styles.plus}
+                      src="/assets/icons/Edit.svg"
+                      alt="edit"
+                    />
+                  </button>
                 </div>
                 <p className={styles.price}>${product.price}</p>
                 <img
